45312663ad79bb7be61c40aa6cb768bb6b2b2804
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-19 20:16:41 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup.test.ts
+++ src/presentation/controllers/signup.test.ts
@@ -25,7 +25,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_email@mail.com',
+        email: 'valid_email@gmail.com',
         password: 'valid_password',
       };
       return fakeAccount;
@@ -196,6 +196,26 @@ describe('SignUp Controller', () => {
       name: 'any_name',
       email: 'any_email@gmail.com',
       password: 'any_password',
+    });
+  });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@gmail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      },
+    };
+    const httResponse = sut.handle(httpRequest);
+    expect(httResponse.statusCode).toBe(200);
+    expect(httResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@gmail.com',
+      password: 'valid_password',
     });
   });
 });
