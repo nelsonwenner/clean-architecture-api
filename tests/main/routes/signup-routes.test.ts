19e8ce92673d7a5ec2c1667e61505d070020d7cd
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-18 20:06:19 -0300

    refactor: connect to mongodb vefore integration tests

--- tests/main/routes/signup-routes.test.ts
+++ tests/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
-import request from 'supertest'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
+import request from 'supertest'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return 200 on signup', async () => {
     await request(app)
       .post('/api/signup')
