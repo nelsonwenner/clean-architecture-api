273146e47a6b580948a40ae908f71068c1903e48
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-18 21:02:56 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,32 @@
 import 'module-alias/register'
-import app from './config/app'
+import env from '@/main/config/env'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
+  )
+  throw reason
+})
+
+process.on('uncaughtException', (error) => {
+  console.error(`App exiting due to an uncaught exception: ${error}`)
+  process.exit(ExitStatus.Failure)
+})
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () =>
+      console.log(`Server start with successfully on PORT ${env.port}`)
+    )
+  })
+  .catch((error) => {
+    console.error(`App exited with error: ${error}`)
+    process.exit(ExitStatus.Failure)
+  })
