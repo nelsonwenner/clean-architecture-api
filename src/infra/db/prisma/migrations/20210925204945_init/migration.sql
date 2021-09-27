f5da7caca855b701642740976d3ecf53ee8cd37b
wenner <nelsondiasdemedeiros@gmail.com>
2021-09-26 21:42:01 -0300

    chore: setup prisma database

---
+++ src/infra/db/prisma/migrations/20210925204945_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_email_key" ON "Account"("email");
