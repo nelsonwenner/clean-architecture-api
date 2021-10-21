6a5a2627af2972574d0bf6fb6c1a501613bb41fb
wenner <nelsondiasdemedeiros@gmail.com>
2021-10-21 14:44:08 -0300

    feat: add new methods in the repository

--- src/infra/db/prisma/account-repository/account.ts
+++ src/infra/db/prisma/account-repository/account.ts
@@ -1,4 +1,10 @@
-import { AddAccountRepository, ExistsAccountRepository } from '@data/contracts'
+import {
+  AddAccountRepository,
+  ExistsAccountRepository,
+  LoadAccountByEmailRepository,
+  LoadAccountByTokenRepository,
+  LoadAccountRepository,
+} from '@data/contracts'
 import { AddAccount } from '@domain/usecases/add-account'
 import { prisma } from '@infra/db/prisma/client'
 import { Account } from '@domain/account/account'
@@ -20,5 +26,38 @@ export class AccountPrismaRepository
       where: { email },
     })
     return !!userExists
+  }
+
+  async load(accountId: string): Promise<LoadAccountRepository.Result> {
+    const { id, name, email } = await prisma.account.findUnique({
+      where: { id: accountId },
+    })
+    return { id, name, email }
+  }
+
+  async loadByEmail(
+    email: string
+  ): Promise<LoadAccountByEmailRepository.Result> {
+    const account = await prisma.account.findUnique({
+      where: { email },
+    })
+    return account
+  }
+
+  async loadByToken(
+    token: string,
+    role?: string
+  ): Promise<LoadAccountByTokenRepository.Result> {
+    const account = await prisma.account.findUnique({
+      where: { accessToken: token },
+    })
+    return account
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    await prisma.account.update({
+      where: { id },
+      data: { accessToken: token },
+    })
   }
 }
