1b259d9bbb67e7b978f96749a771d1ffe5f4cd5f
wenner <nelsondiasdemedeiros@gmail.com>
2021-10-28 14:49:06 -0300

    add improve in the route adapter

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,12 +1,14 @@
-import { Controller, HttpRequest } from '@presentation/contracts'
+import { Controller } from '@presentation/contracts'
 import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body,
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {}),
+      accountId: req.accountId,
     }
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await controller.handle(request)
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
