41b585936776564950b6dbdc95d5a460247fc447
wenner <nelsondiasdemedeiros@gmail.com>
2021-11-11 14:55:31 -0300

    delete file

--- src/main/routes/signup-routes.ts
+++
@@ -1,7 +0,0 @@
-import { adaptRoute } from '@main/adapters/express-route-adapter'
-import { makeSignUpController } from '@main/factories/controllers'
-import { Router } from 'express'
-
-export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-}
